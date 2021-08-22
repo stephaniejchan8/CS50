06ee1e2a83db830339409b9aa7114ce93aaff568
shtoophy <shtoophy@users.noreply.github.com>
2021-08-22 23:39:13 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -18,7 +18,6 @@ int main(void)
             letters++;
         }
     }
-    printf("Number of letters in text: %i\n", letters);
     // Calculate number of words in text.
     int words = 0;
     for (int i = 0; i < n; i++)
@@ -32,7 +31,6 @@ int main(void)
     {
         words++;
     }
-    printf("Number of words in text: %i\n", words);
 
     // Calculate number of sentences in text.
     int sentences = 0;
@@ -43,19 +41,15 @@ int main(void)
             sentences++;
         }
     }
-    printf("Number of sentences in text: %i\n", sentences);
 
     // Calculate average number of letters per 100 words.
     double avletters = letters / (double) words * 100;
-    printf("Average number of letters per 100 words: %f\n", avletters);
 
     // Calculate average number of sentences per 100 words.
     double avsentences = sentences / (double) words * 100;
-    printf("Average number of sentences per 100 words: %f\n", avsentences);
 
     // Input averages into index.
     double index = 0.0588 * avletters - 0.296 * avsentences - 15.8;
-    printf("Index: %f\n", index);
 
     // Calculate and print out reading level.
     int level = (int) round(index);
