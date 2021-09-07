7642322b9c780375e390bf8b6d94d55dc97d1396
shtoophy <shtoophy@users.noreply.github.com>
2021-09-07 23:00:32 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -121,12 +121,9 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count - 1; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = i + 1; j < candidate_count; j++)
         {
-            if (i < j)
-            {
-                preferences[ranks[i]][ranks[j]]++;
-            }
+            preferences[ranks[i]][ranks[j]]++;
         }
     }
     return;
