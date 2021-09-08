6c1d5a17df8b9d4b732eed003c487d242c7e9665
shtoophy <shtoophy@users.noreply.github.com>
2021-09-08 00:11:21 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -134,12 +134,18 @@ void add_pairs(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = i + 1; j < candidate_count; j++)
         {
             if (preferences[i][j] > preferences[j][i])
             {
                 pairs[pair_count].winner = i;
                 pairs[pair_count].loser = j;
+                pair_count++;
+            }
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
                 pair_count++;
             }
         }
