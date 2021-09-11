f61b8332c4eb5f8de815de91955bcfdd92eb9016
shtoophy <shtoophy@users.noreply.github.com>
2021-09-11 06:44:56 +0000

    automated commit by check50 [check50=True]

---
+++ volume.c
@@ -0,0 +1,60 @@
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // Copy header from input file to output file
+    uint8_t *header = malloc(HEADER_SIZE * sizeof(uint8_t));
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
+
+    free(header);
+
+    // Read samples from input file and write updated data to output file
+    int16_t *sample = malloc(sizeof(int16_t));
+    int indicator;
+    while ((indicator = feof(input)) == 0)
+    {
+        fread(sample, sizeof(int16_t), 1, input);
+        *sample = *sample * factor;
+        fwrite(sample, sizeof(int16_t), 1, output);
+    }
+
+
+    // Close files
+    fclose(input);
+    fclose(output);
+    free(sample);
+    return 0;
+}
