3cc349043f2f38b4f226d6b7a2f75f11fd2c32a6
shtoophy <shtoophy@users.noreply.github.com>
2021-09-11 07:23:20 +0000

    automated commit by check50 [check50=True]

--- volume.c
+++ volume.c
@@ -34,21 +34,16 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // Copy header from input file to output file
-    uint8_t *header = malloc(HEADER_SIZE * sizeof(uint8_t));
-    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
 
-    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
-
-    free(header);
+    fwrite(header, HEADER_SIZE, 1, output);
 
 
     // Read samples from input file and write updated data to output file
     int16_t buffer;
-    int indicator;
-
-    while ((indicator = feof(input)) == 0)
+    while (fread(&buffer, sizeof(int16_t), 1, input))
     {
-        fread(&buffer, sizeof(int16_t), 1, input);
         buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
     }
@@ -57,5 +52,4 @@ int main(int argc, char *argv[])
     // Close files
     fclose(input);
     fclose(output);
-
 }
